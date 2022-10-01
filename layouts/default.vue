c60ab1ba034038704fba6dbf69c7de8ef56f650c
Dobbs <benjamindobbs@gmail.com>
2022-09-30 22:26:42 -0400

    added basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          
+        >
+          <v-list-item-action>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  v-bind="attrs"
+                  v-on="on">
+                  {{ item.icon }}
+                </v-icon>
+              </template>
+                <span> {{item.title}} </span>
+              </v-tooltip>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-home-outline',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: ' mdi-tower-beach',
+          title: 'Catapult',
+          to: '/catapult'
+        },
+        {
+          icon: 'mdi-bridge',
+          title: 'Paper Bridges',
+          to: '/bridge'
+        },
+        {
+          icon: 'mdi-domino-mask',
+          title: 'Mask',
+          to: '/mask'
+        },
+        {
+          icon: 'mdi-kite-outline',
+          title: 'Kite',
+          to: '/kite'
+        },
+        {
+          icon: 'mdi-sofa-single-outline',
+          title: 'Cardboard Funiture',
+          to: '/cardboar-furniture'
+        },
+        {
+          icon: 'mdi-shape-outline',
+          title: 'Geometric Art',
+          to: '/geometric-art'
+        },
+        {
+          icon: 'mdi-language-python',
+          title: 'Final Project',
+          to: '/final-project'
+        }
+      ],
+      miniVariant: true,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/catapult.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-main>
+        <v-carousel hide-delimiters :show-arrows="false" cycle>
+            <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+                
+            ></v-carousel-item>
+        </v-carousel>
+  <v-container>
+    <v-row
+    justify="space-around" class="pa-12">
+        <v-card>
+            <v-card-text>
+                Lorem Ipsum
+            </v-card-text>
+        </v-card>
+        <v-card>
+            <v-card-text>
+                Lorem Ipsum
+            </v-card-text>
+        </v-card>
+        <v-card>
+            <v-card-text>
+                Lorem Ipsum
+            </v-card-text>
+        </v-card>
+    </v-row>
+    <v-spacer></v-spacer>
+    <v-card class="ma-6">
+        <v-card-title>
+            Description
+        </v-card-title>
+        <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+    </v-card>
+    <v-card class="ma-6">
+        <v-card-title>
+        Process
+        </v-card-title>
+        <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+        <v-carousel>
+            <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+            ></v-carousel-item>
+        </v-carousel>
+    </v-card>
+    <v-card class="ma-6">
+        <v-card-title>
+          Results  
+        </v-card-title>
+        <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+        <v-carousel>
+            <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+            ></v-carousel-item>
+        </v-carousel>
+    </v-card>
+  </v-container>
+    </v-main>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            name: 'catapult',
+            items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          }
+        ]
+        }
+        }
+    }
+</script>
